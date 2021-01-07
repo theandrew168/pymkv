5989eb3af9eeff9d757929d9617399e9f2ac13b4
theandrew168 <steveday168@gmail.com>
2021-01-06 19:21:36 -0600

    Remove docs from main.py

--- main.py
+++ main.py
@@ -96,22 +96,6 @@ def key2volume(key, volume_names, replicas, subvolumes):
     return volumes
 
 
-"""
-Volume "servers" store all of the actual data. The data is evenly
-dispersed into a predictable nested directory structure based on
-the base64'd hash of its contents. Could be text, a file, doesn't
-really matter. It is all bytes. This part of the program is just
-an NGINX subprocess. The config is generated as needed, thrown into
-a tempfile, and started as a child process. NGINX then starts up
-its own worker child processes.
-
-The index server manages a mapping of where stuff lives on the volume
-servers. LevelDB (via plyvel) is store this mapping in a fast, persistent
-way. The index server also exposes the primary API. GET gets data, PUT
-stores data, DELETE deletes data. Pretty simple! The index server talks
-with the volume servers in a similar manner.
-"""
-
 class Application:
 
     def __init__(self, db, volume_names, replicas, subvolumes):
